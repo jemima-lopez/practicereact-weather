9b027f092f7b05edfbbe184121bddc995831f12e
Jemima <jlopezj11@miumg.edu.gt>
2025-01-17 21:29:00 -0600

    commit 2

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,29 @@
+import { Box, Container, TextField, Typography } from "@mui/material";
 
 export default function App() {
+
+  const onSubmit = (event) => { e.preventDeFault(); console.log("submit"); };
+
   return (
-    <div>App</div>
-  )
+    <Container maxWidth="xs" sx={{ mt: 2 }}>
+      <Typography variant="h3" component={"h1"} align="center" gutterBottom>
+        Wheather App
+      </Typography>
+      <Box
+        sx={{ display: "grid", gap: 2 }}
+        component="form"
+        autoComplete="off"
+        onSubmit={onSubmit}
+      >
+        <TextField
+          id="city"
+          label="City"
+          variant="outlined"
+          size="small"
+          required
+          fullWidth
+        />
+      </Box>
+    </Container>
+  );
 }
-
-

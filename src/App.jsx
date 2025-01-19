d95e82a53db5921077da097a141dd8c28755d272
Jemima <jlopezj11@miumg.edu.gt>
2025-01-18 18:50:27 -0600

    commit 3

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,27 @@
+import { LoadingButton } from "@mui/lab";
 import { Box, Container, TextField, Typography } from "@mui/material";
+import { green } from "@mui/material/colors";
+import { useState } from "react";
+
+const API_WEATHER =
+  "https://api.weatherapi.com/v1/current.json?key=${import.meta.env.VITE_APP_WEATHER_API_KEY}&q=${city}";
 
 export default function App() {
+  const [city, setCity] = useState("");
+  const [loading, setLoading] = useState(false);
 
-  const onSubmit = (event) => { e.preventDeFault(); console.log("submit"); };
+  const onSubmit = async (e) => {
+    e.preventDeFault();
+    console.log("submit");
+    setLoading(true);
+    try {
+      if (!city.trim()) throw { message: "City is required" };
+    } catch (error) {
+      console.error(error);
+    } finally {
+      setLoading(false);
+    }
+  };
 
   return (
     <Container maxWidth="xs" sx={{ mt: 2 }}>
@@ -22,8 +41,26 @@ export default function App() {
           size="small"
           required
           fullWidth
+          value={city}
+          onChange={(e) => setCity(e.target.value)}
         />
+        <LoadingButton
+          type="submit"
+          variant="contained"
+          sx={{ color: "white", "&:hover": { backgroundColor: green[700] } }}
+          loading={loading}
+          loadingIndicator="loading..."
+          fullWidth
+        >
+          Search
+        </LoadingButton>
       </Box>
+      <Typography textAlign={"center"} sx={{ mt: 2, fontSize: 10 }}>
+        Power by{" "}
+        <a href="https://www.weatherapi.com/" title="Weathert API">
+          WeatherAPI.com
+        </a>
+      </Typography>
     </Container>
   );
 }

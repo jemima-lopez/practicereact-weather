af9376b0b4c5fc63658f465413550acd5aacd387
Jemima <jlopezj11@miumg.edu.gt>
2025-01-20 22:01:08 -0600

    commit 7

--- src/main.jsx
+++ src/main.jsx
@@ -1,18 +1,18 @@
 import React from "react";
-import { SnackbarProvider } from "notistack";
-import { createRoot } from "react-dom/client";
+import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
-import "./index.css";
+
 import "@fontsource/roboto/300.css";
 import "@fontsource/roboto/400.css";
 import "@fontsource/roboto/500.css";
 import "@fontsource/roboto/700.css";
-
+import "./index.css";
 
 import { CssBaseline } from "@mui/material";
+import { SnackbarProvider } from "notistack";
 
-const root = createRoot(document.getElementById('root'));
-root.render(
+ReactDOM.createRoot(document.getElementById('root')).render
+(
   <React.StrictMode>
     <SnackbarProvider>
       <CssBaseline />

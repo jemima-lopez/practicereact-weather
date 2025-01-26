2e4186258c745ce1354376ea71dfbdcf653d880d
Jemima <jlopezj11@miumg.edu.gt>
2025-01-25 21:36:18 -0600

    commit 8

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,10 @@ import { LoadingButton } from "@mui/lab";
 import { Box, Container, TextField, Typography } from "@mui/material";
 import { green } from "@mui/material/colors";
 import { useState } from "react";
-import { data } from "react-router-dom";
 
-const API_WEATHER =
-  "https://api.weatherapi.com/v1/current.json?key=${import.meta.env.VITE_API_KEY}&q=";
+
+const API_WEATHER = "https://api.weatherapi.com/v1/current.json?key=${import.meta.env.VITE_API_KEY}&lang=es&q=";
+
 
 export default function App() {
   const [city, setCity] = useState("");
@@ -30,18 +30,20 @@ export default function App() {
 
     try {
       if (!city.trim()) throw { message: "City is required" };
+
       const response = await fetch(`${API_WEATHER}${city}`);
       const data = await response.json();
-      if (data.error) throw { message: data.error.message };
 
+      if (data.error) throw { message: data.error.message };
+      console.log(data);
       setWeather({
         city: data.location.name,
-    country: data.location.country,
-    temp: data.current.temp_c,
-    condition: data.current.condition.text,
-    icon: data.current.condition.icon,
-    conditinoText: data.current.condition.text,
-  });
+        country: data.location.country,
+        temp: data.current.temp_c,
+        condition: data.current.condition.code,
+        icon: data.current.condition.icon,
+        conditinoText: data.current.condition.text,
+      });
     } catch (error) {
       setError({ error: true, message: error.message });
     } finally {
@@ -98,6 +100,45 @@ export default function App() {
             Search
           </LoadingButton>
         </Box>
+
+
+        {weather.city && (
+        <Box
+          sx={{
+            mt: 2,
+            display: "grid",
+            gap: 2,
+            textAlign: "center",
+          }}
+        >
+          <Typography
+            variant="h4"
+            component="h2"
+          >
+            {weather.city}, {weather.country}
+          </Typography>
+          <Box
+            component="img"
+            alt={weather.conditionText}
+            src={weather.icon}
+            sx={{ margin: "0 auto" }}
+          />
+          <Typography
+            variant="h5"
+            component="h3"
+          >
+            {weather.temperature} °C
+          </Typography>
+          <Typography
+            variant="h6"
+            component="h4"
+          >
+            {weather.conditionText}
+          </Typography>
+        </Box>
+      )}
+
+
         <Typography textAlign={"center"} sx={{ mt: 2, fontSize: 10 }}>
           Power by{" "}
           <a href="https://www.weatherapi.com/" title="Weathert API">
--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
+import App from "./App";
 
 import "@fontsource/roboto/300.css";
 import "@fontsource/roboto/400.css";
@@ -11,8 +11,7 @@ import "./index.css";
 import { CssBaseline } from "@mui/material";
 import { SnackbarProvider } from "notistack";
 
-ReactDOM.createRoot(document.getElementById('root')).render
-(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <SnackbarProvider>
       <CssBaseline />
@@ -20,4 +19,3 @@ ReactDOM.createRoot(document.getElementById('root')).render
     </SnackbarProvider>
   </React.StrictMode>
 );
-

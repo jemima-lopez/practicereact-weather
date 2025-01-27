e8f65a2d2edbbe8007a784141eafbaac1a1c4043
Jemima <jlopezj11@miumg.edu.gt>
2025-01-26 21:11:24 -0600

    commit 9

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,9 @@
 import { LoadingButton } from "@mui/lab";
 import { Box, Container, TextField, Typography } from "@mui/material";
-import { green } from "@mui/material/colors";
 import { useState } from "react";
 
-
-const API_WEATHER = "https://api.weatherapi.com/v1/current.json?key=${import.meta.env.VITE_API_KEY}&lang=es&q=";
-
+const API_WEATHER =
+  "http://api.weatherstack.com/current?access_key=9e99fd7782f71778f19351a1c5454006&query=";
 
 export default function App() {
   const [city, setCity] = useState("");
@@ -17,104 +15,95 @@ export default function App() {
   const [weather, setWeather] = useState({
     city: "",
     country: "",
-    temp: "",
-    condition: "",
+    temperature: 0,
     icon: "",
-    conditinoText: "",
+    conditionText: "",
   });
 
   const onSubmit = async (e) => {
-    e.preventDeFault();
-    setLoading(true);
+    e.preventDefault(); // Corregido
     setError({ error: false, message: "" });
+    setLoading(true);
 
     try {
       if (!city.trim()) throw { message: "City is required" };
 
-      const response = await fetch(`${API_WEATHER}${city}`);
-      const data = await response.json();
+      const res = await fetch(`${API_WEATHER}${city}`);
+      const data = await res.json();
 
-      if (data.error) throw { message: data.error.message };
-      console.log(data);
+      if (data.error) {
+        throw { message: data.error.info };
+      }
+
       setWeather({
         city: data.location.name,
         country: data.location.country,
-        temp: data.current.temp_c,
-        condition: data.current.condition.code,
-        icon: data.current.condition.icon,
-        conditinoText: data.current.condition.text,
+        temperature: data.current.temperature, // Ajustado
+        icon: data.current.weather_icons[0], // Ajustado
+        conditionText: data.current.weather_descriptions[0], // Ajustado
       });
     } catch (error) {
-      setError({ error: true, message: error.message });
+      setError({ error: true, message: error.message || "Something went wrong" });
     } finally {
       setLoading(false);
     }
   };
 
   return (
-    <>
-      <Container
-        maxWidth="xs"
-        sx={{ mt: 8 }}
-        style={{
-          width: "500px",
-          height: "300px",
-          borderRadius: "8px",
-          backgroundColor: "rgba(255, 255, 255, 0.015)", // Fondo semitransparente
-          backdropFilter: "blur(10px)", // Aplicar desenfoque
-          boxShadow: "0 4px 6px rgba(0, 0, 0, 0.1)", // Sombra
-        }}
+    <Container
+      maxWidth="xs"
+      sx={{ mt: 8 }}
+      style={{
+        width: "600px",
+        height: "500px",
+        borderRadius: "8px",
+        backgroundColor: "rgba(255, 255, 255, 0.015)",
+        backdropFilter: "blur(10px)",
+        boxShadow: "0 4px 6px rgba(0, 0, 0, 0.1)",
+      }}
+    >
+      <Typography variant="h3" component="h1" align="center" gutterBottom>
+        Weather App
+      </Typography>
+      <Box
+        sx={{ display: "grid", gap: 2 }}
+        component="form"
+        autoComplete="off"
+        onSubmit={onSubmit}
       >
-        <Typography variant="h3" component="h1" align="center" gutterBottom>
-          Wheather App
-        </Typography>
-        <Box
-          sx={{ display: "grid", gap: 2 }}
-          component="form"
-          autoComplete="off"
-          onSubmit={onSubmit}
+        <TextField
+          id="city"
+          label="City"
+          variant="outlined"
+          size="small"
+          required
+          fullWidth
+          value={city}
+          onChange={(e) => setCity(e.target.value)}
+          error={error.error}
+          helperText={error.message}
+        />
+        <LoadingButton
+          type="submit"
+          variant="contained"
+          loading={loading}
+          loadingIndicator="loading..."
         >
-          <TextField
-            id="city"
-            label="City"
-            variant="outlined"
-            size="small"
-            required
-            fullWidth
-            value={city}
-            onChange={(e) => setCity(e.target.value)}
-            error={error.error}
-            helperText={error.message}
-          />
-          <LoadingButton
-            type="submit"
-            variant="contained"
-            sx={{
-              color: "white",
-              "&:hover": { backgroundColor: green[700] },
-            }}
-            loading={loading}
-            loadingIndicator="loading..."
-            fullWidth
-          >
-            Search
-          </LoadingButton>
-        </Box>
+          Search
+        </LoadingButton>
+      </Box>
 
-
-        {weather.city && (
+      {weather.city && (
         <Box
           sx={{
             mt: 2,
             display: "grid",
             gap: 2,
             textAlign: "center",
+            
           }}
         >
-          <Typography
-            variant="h4"
-            component="h2"
-          >
+          <Typography variant="h4" component="h2">
             {weather.city}, {weather.country}
           </Typography>
           <Box
@@ -123,29 +112,21 @@ export default function App() {
             src={weather.icon}
             sx={{ margin: "0 auto" }}
           />
-          <Typography
-            variant="h5"
-            component="h3"
-          >
+          <Typography variant="h5" component="h3" >
             {weather.temperature} °C
           </Typography>
-          <Typography
-            variant="h6"
-            component="h4"
-          >
+          <Typography variant="h6" component="h4">
             {weather.conditionText}
           </Typography>
         </Box>
       )}
 
-
-        <Typography textAlign={"center"} sx={{ mt: 2, fontSize: 10 }}>
-          Power by{" "}
-          <a href="https://www.weatherapi.com/" title="Weathert API">
-            WeatherAPI.com
-          </a>
-        </Typography>
-      </Container>
-    </>
+      <Typography textAlign="center" sx={{ mt: 2, fontSize: 10 }}>
+        Power by{" "}
+        <a href="https://weatherstack.com/" title="Weather API">
+          weatherstack.com
+        </a>
+      </Typography>
+    </Container>
   );
 }
